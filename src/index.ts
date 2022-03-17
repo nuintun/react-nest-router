bef6433c3711b0cb44c71d9fb179ff8b1d03dbaa
nuintun <nuintun@qq.com>
2022-03-17 17:34:20 +0800

    ✨ Update code

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@
  * @module index
  */
 
+export * from './hooks/useRouter';
 export * from './hooks/useOutlet';
 export * from './hooks/useParams';
-export * from './hooks/useRouter';
-export * from './components/Outlet';
 export * from './components/Router';
-export * from './hooks/useLocation';
+export * from './components/Outlet';
 export * from './hooks/useNavigate';
+export * from './hooks/useLocation';
 export * from './hooks/useOutletContext';
-export { OutletProps, Route, RouteMatch } from './types';
+export type { OutletProps, Route, RouteMatch } from './types';
