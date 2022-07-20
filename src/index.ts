28ad7a74b7f011d069ae24ce837ef582fee1eb37
nuintun <nuintun@qq.com>
2022-07-20 14:50:11 +0800

    feat: Export Navigate component

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
  * @module index
  */
 
+// Types
 export type {
   Action,
   IRoute,
@@ -18,16 +19,23 @@ export type {
   RouterProps,
   To
 } from './types';
+
+// Hooks
 export * from './hooks/useMatch';
 export * from './hooks/useRouter';
 export * from './hooks/useOutlet';
 export * from './hooks/useParams';
 export * from './hooks/useResolve';
 export * from './hooks/useMatches';
-export * from './components/Router';
-export * from './components/Outlet';
 export * from './hooks/useNavigate';
 export * from './hooks/useLocation';
 export * from './hooks/useMatchIndex';
 export * from './hooks/useOutletContext';
+
+// Components
+export * from './components/Router';
+export * from './components/Outlet';
+export * from './components/Navigate';
+
+// Utils
 export { join, normalize, resolve } from './path';
