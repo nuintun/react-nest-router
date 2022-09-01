208c461676200c5cf958cfd2d5fc2ba747409fd3
nuintun <nuintun@qq.com>
2022-09-01 15:48:21 +0800

    feat: Update hooks

--- src/hooks/useSyncRef.ts
+++ src/hooks/useSyncRef.ts
@@ -18,9 +18,7 @@ export function useSyncRef<T>(value: T): React.MutableRefObject<T>;
 export function useSyncRef<T = undefined>(value?: T): React.MutableRefObject<T | undefined> {
   const valueRef = useRef(value);
 
-  // https://github.com/reactjs/rfcs/pull/220
   // https://github.com/alibaba/hooks/issues/728
-  // https://github.com/facebook/react/issues/20394
   valueRef.current = useMemo(() => value, [value]);
 
   return valueRef;
