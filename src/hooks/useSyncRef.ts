634e5b8824ef1932ba5f9722ad40b54651e9807e
nuintun <nuintun@qq.com>
2022-09-01 17:12:44 +0800

    feat: Update hooks

--- src/hooks/useSyncRef.ts
+++ src/hooks/useSyncRef.ts
@@ -12,7 +12,7 @@ export function useSyncRef<T = undefined>(): React.MutableRefObject<T | undefine
 /**
  * @function useSyncRef
  * @description Create self sync ref.
- * @param value Ref value
+ * @param value Value.
  */
 export function useSyncRef<T>(value: T): React.MutableRefObject<T>;
 export function useSyncRef<T = undefined>(value?: T): React.MutableRefObject<T | undefined> {
