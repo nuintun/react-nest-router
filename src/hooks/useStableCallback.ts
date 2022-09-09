4a71c2cbc7868824c9cc62d554a14408574c6133
nuintun <nuintun@qq.com>
2022-09-09 09:24:45 +0800

    feat: Update hooks

--- src/hooks/useStableCallback.ts
+++ src/hooks/useStableCallback.ts
@@ -5,12 +5,20 @@
 import { useCallback } from 'react';
 import { useSyncRef } from './useSyncRef';
 
+export interface Callback {
+  (this: any, ...args: any[]): any;
+}
+
+interface CallbackFunction {
+  (this: unknown, ...args: unknown[]): unknown;
+}
+
 /**
  * @function useStableCallback
  * @description Create a stable callback.
  * @param callback Callback.
  */
-export function useStableCallback<C extends (...args: any[]) => any = (...args: unknown[]) => unknown>(callback: C): C {
+export function useStableCallback<C extends Callback = CallbackFunction>(callback: C): C {
   const callbackRef = useSyncRef(callback);
 
   return useCallback(((...args) => callbackRef.current(...args)) as C, []);
